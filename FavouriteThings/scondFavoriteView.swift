e7eec09a991d4668094840d4a594e994657dd2ea
Nicole Huang <nhuang@lcs.on.ca>
2021-10-22 08:54:54 -0400

    Added images of three favorite things, and got them to show up on the phone.

---
+++ FavouriteThings/scondFavoriteView.swift
@@ -0,0 +1,22 @@
+//
+//  scondFavoriteView.swift
+//  FavouriteThings
+//
+//  Created by Nicole on 2021/10/20.
+//
+
+import SwiftUI
+
+struct scondFavoriteView: View {
+    var body: some View {
+        Image("icecream")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+struct scondFavoriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        scondFavoriteView()
+    }
+}
